b62f50893762bef012157dc41b638cf17bfc0c02
hyhae <helsherif95@gmail.com>
2023-07-29 19:46:26 +0300

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+    <title>Hussein - Transforming businesses</title>
+</svelte:head>
+
+
+<nav>
+    <a>Hussein.</a>
+    <ul>
+        <a></a>
+        <a></a>
+        <a></a>
+        <a></a>
+        <a></a>
+    </ul>
+</nav>
